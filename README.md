bc4afbf520bdeceddb00f700de31c075d47b0f5a
Brett Taylor <brett@webfroot.co.nz>
2014-08-14 17:05:17 +1200

    make default environment local, unless _LARAVEL_TOWERFELLOWS_ENV is set.

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ## Towerfellows
 
 An opinionated ELO system for an excellent local-multiplayer game.
+
+### Local Setup
+
+`bootstrap/start.php` has a closure that sets the environment - it will default to local unless you set it yourself with an environment variable `_LARAVEL_TOWERFELLOWS_ENV`.
+
+```sh
+export _LARAVEL_TOWERFELLOWS_ENV=local
+```
--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,11 +24,12 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-$env = $app->detectEnvironment(array(
+$env = $app->detectEnvironment(function()
+{
 
-	'local' => array('homestead'),
+    return ( getenv('_LARAVEL_TOWERFELLOWS_ENV') ? getenv('_LARAVEL_TOWERFELLOWS_ENV') : 'local' );
 
-));
+});
 
 /*
 |--------------------------------------------------------------------------
